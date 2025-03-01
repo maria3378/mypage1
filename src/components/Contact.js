b73c472463603da4c4b875fdcc47b808c84db628
Maria Eberechi Kalu Ugwu <145647682+maria3378@users.noreply.github.com>
2025-03-01 22:02:48 +0200

    Update Contact.js

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section id="contact" style={{ background: "#bbb", padding: "80px 20px", height: "100vh", textAlign: "center" }}>
+      <h1>Contact</h1>
+      <p>Email: example@email.com</p>
+      <p>Phone: +123 456 7890</p>
+    </section>
+  );
+};
+
+export default Contact;
